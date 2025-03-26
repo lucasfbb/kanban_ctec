e2a86b7727ccaead76f13a7c72dfd49f66310663
lucasfbb <lucasfirmino2612@gmail.com>
2025-03-26 17:09:59 -0300

    add

--- src/pages/Boards/index.tsx
+++ src/pages/Boards/index.tsx
@@ -1,6 +1,17 @@
+import {DndContext} from '@dnd-kit/core';
+import {Draggable} from './Draggable';
+import {Droppable} from './Droppable';
 
 const Boards = () => {
-    return <div>Boards</div>
+    return (
+        <>
+            <DndContext onDragEnd={(result: any) => console.log(result)}>
+                <div className=''></div>
+            </DndContext>
+        
+        
+        </>
+    )
 }
 
 export default Boards;
